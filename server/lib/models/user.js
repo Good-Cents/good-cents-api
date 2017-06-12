8a302229431fd6e345770c515abdd8093001ed4e
Chris_Wallace <kawallis@icloud.com>
2017-06-12 13:30:30 -0700

    added auth to schema

--- server/lib/models/user.js
+++ server/lib/models/user.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
@@ -7,7 +8,17 @@ const schema = new Schema({
   name: String,
   email: String,
   password: String,
+  hash: String,
   goals: [{type: Schema.Types.ObjectId, ref: 'Goal'}]
+});
+
+//Methods below are for app encryption
+schema.method('generateHash', function (password) {
+  this.hash = bcrypt.hashSync(password, 8);
+});
+
+schema.method('comparePassword', function (password) {
+  return bcrypt.compareSync(password, this.hash);
 });
 
 module.exports = mongoose.model('User', schema);
