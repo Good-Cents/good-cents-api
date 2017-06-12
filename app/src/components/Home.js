cde7c0440d226c12c3378435665daf699dac595e
Chris_Wallace <kawallis@icloud.com>
2017-06-12 13:52:47 -0700

    added a couple boiler components and a auth route

---
+++ app/src/components/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+
+  render() {
+    return (
+      <div className="Home">
+        <h1>Welcome to GoodCents</h1>
+      </div>
+    );
+  }
+}
+
+export default Home;
