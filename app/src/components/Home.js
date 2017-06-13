cf9963e8d2d0066ad605c4ed82e7cb5a7e5e6c48
Chris_Wallace <kawallis@icloud.com>
2017-06-13 15:22:03 -0700

    added carosal and pig

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import BankLink from '../Plaid/BankLink';
 import Credentials from '../components/Credentials';
 import saveYourWay from '../photos/saveYourWay.jpg';
+import '../css/Home.css';
 
 class Home extends Component {
   render() {
     return (
-    <div className="Home">
-        <img src={saveYourWay} alt="backpack" />
+    <div className="Home"> 
+            <img className="backpack" src={saveYourWay} alt="backpack" />
             <div className="welcomeHome">
                 <h2 className="dash-trailing">This Is Good Cents.</h2>
                 <p>One taste silent disco beltane, patchouli retreat. Tahini Hafiz tofu, ancient toxins rites of passage surrender chia seeds natural homebirth om Dr. Bronner's. Doula ecstatic dance transformative solstice third eye midwifery higher cosmic force medical marijuana papyrus font, organic raw cacao. Namaste. A Simple Shared account helps you and your partner easily budget, save, and talk about money. In good financial partnership, one plus one can equal anything.</p>
