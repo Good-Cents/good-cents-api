7652ffc692005c652b03737de8dd418e390a9389
Ivy Patton <lilaivy@gmail.com>
2017-06-13 01:05:46 -0700

    changed font and styled intro div

--- app/src/App.js
+++ app/src/App.js
@@ -32,7 +32,7 @@ class App extends Component {
           <img src={saveYourWay} alt="backpack" />
          <div className="welcomeHome">
             <h2 className="dash-trailing">This Is Good Cents.</h2>
-            <p>One taste silent disco beltane, patchouli retreat. Tahini Hafiz tofu, ancient toxins rites of passage surrender chia seeds natural homebirth om Dr. Bronner's. Doula ecstatic dance transformative solstice third eye midwifery higher cosmic force medical marijuana papyrus font, organic raw cacao. Namaste.</p>
+            <p>One taste silent disco beltane, patchouli retreat. Tahini Hafiz tofu, ancient toxins rites of passage surrender chia seeds natural homebirth om Dr. Bronner's. Doula ecstatic dance transformative solstice third eye midwifery higher cosmic force medical marijuana papyrus font, organic raw cacao. Namaste. A Simple Shared account helps you and your partner easily budget, save, and talk about money. In good financial partnership, one plus one can equal anything.</p>
           </div>
         
 
