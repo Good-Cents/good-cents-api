56407d19a2a726bc06d20fe487a60ce37098d866
Ivy Patton <lilaivy@gmail.com>
2017-06-12 23:24:17 -0700

    deleted react logo

--- app/src/App.js
+++ app/src/App.js
@@ -26,7 +26,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src="/assets/images/svg/we-got-your-back.svg" className="App-logo" alt="logo" />
           <h2>Good Cents API</h2>
         </div>
         <div className="App-intro">
