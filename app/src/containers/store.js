545e72ec599a79f7dc6a82e090de60b7a6981f19
Ivy Patton <lilaivy@gmail.com>
2017-06-13 15:54:24 -0700

    added redux devtools

--- app/src/containers/store.js
+++ app/src/containers/store.js
@@ -1,11 +1,14 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../components/Main/reducers';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const store = createStore(
   rootReducer,
+  composeWithDevTools(applyMiddleware(thunk)),
   {},
-  applyMiddleware(thunk)
 );
+
+
 
 export default store;
